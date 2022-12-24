dcbeeca3e5e06b30efccddc6d9b3bd5e2bbee963
Kenox <muranelp@gmail.com>
2022-12-24 05:46:43 +0100

    Initial Commit

---
+++ mcviewer/mcviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mcviewerApp.swift
+//  mcviewer
+//
+//  Created by Oualid Hbib on 24.12.22.
+//
+
+import SwiftUI
+
+@main
+struct mcviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
